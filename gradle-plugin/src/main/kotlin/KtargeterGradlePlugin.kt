88c376e7476f703b8fc047b49a3ee98e842f2c6d
Sasha Shpota <5640984+Shpota@users.noreply.github.com>
2021-01-24 13:04:46 +0200

    Use updated Gradle API (#12)

--- gradle-plugin/src/main/kotlin/KtargeterGradlePlugin.kt
+++ gradle-plugin/src/main/kotlin/KtargeterGradlePlugin.kt
@@ -1,13 +1,48 @@
 package org.ktargeter
 
-import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.provider.Provider
+import org.jetbrains.kotlin.gradle.plugin.KotlinCompilation
+import org.jetbrains.kotlin.gradle.plugin.KotlinCompilerPluginSupportPlugin
+import org.jetbrains.kotlin.gradle.plugin.SubpluginArtifact
+import org.jetbrains.kotlin.gradle.plugin.SubpluginOption
 
-open class KtargeterGradlePlugin : Plugin<Project> {
-    override fun apply(project: Project) {
-        project.extensions.create(
+class KtargeterGradlePlugin : KotlinCompilerPluginSupportPlugin {
+
+    override fun isApplicable(kotlinCompilation: KotlinCompilation<*>): Boolean =
+        kotlinCompilation.target.project.plugins.hasPlugin(KtargeterGradlePlugin::class.java)
+
+    override fun applyToCompilation(kotlinCompilation: KotlinCompilation<*>): Provider<List<SubpluginOption>> {
+        val project = kotlinCompilation.target.project
+        val extension = project.extensions.findByType(
+            KtargeterGradleExtension::class.java
+        ) ?: KtargeterGradleExtension()
+        val supportedTargets = setOf("get", "set", "field")
+        extension.annotations.values.toSet().forEach {
+            if (it !in supportedTargets) error("annotation target $it is not supported")
+        }
+        if (extension.enabled && extension.annotations.isEmpty()) {
+            error("ktargeter is enabled, but no annotations were set")
+        }
+        val annotationOptions = extension.annotations.map {
+            SubpluginOption(key = "ktargeterAnnotation", value = "${it.value}:${it.key}")
+        }
+        val enabledOption = SubpluginOption(key = "enabled", value = extension.enabled.toString())
+        return project.provider { annotationOptions + enabledOption }
+    }
+
+    override fun apply(target: Project): Unit = with(target) {
+        extensions.create(
             "ktargeter",
             KtargeterGradleExtension::class.java
         )
     }
+
+    override fun getCompilerPluginId() = "ktargeter"
+
+    override fun getPluginArtifact() = SubpluginArtifact(
+        groupId = "org.ktargeter",
+        artifactId = "kotlin-plugin",
+        version = "0.1.0"
+    )
 }
